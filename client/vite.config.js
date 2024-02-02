8a88e8bef2b48ae9ca4f405d90dcc3df60f4cd58
Oskari Saario <saario.oskari@gmail.com>
2024-02-02 10:59:21 +0200

    changed vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: '/',
     plugins: [react()],
     server: {    
-        // this ensures that the browser opens upon server start
-        open: true,
-        // this sets a default port to 3000  
-        port: 3000, 
+      proxy: {
+        'http://localhost:3000': {
+          secure: false,
+        },
+      } 
     },
     esbuild: {
       include: /\.[jt]sx?$/,
