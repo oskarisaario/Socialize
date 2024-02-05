46d3b4e6effad4898998bf7096f9095fd74e4c76
Oskari Saario <saario.oskari@gmail.com>
2024-02-05 12:22:24 +0200

    changed socket address on client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
     if (!socket.current && isAuth) {
       console.log('app.jsx yrittää connectaa socket')
       //socket.current = io("ws://localhost:3002", {transports: ['websocket', 'polling', 'flashsocket']});
-      socket.current = io("wss://socialize-0746.onrender.com", {transports: ['websocket', 'polling', 'flashsocket']});
+      socket.current = io("https://socialize-0746.onrender.com", {transports: ['websocket', 'polling', 'flashsocket']});
       socket.current.emit('addUser', user._id);
     }
     if (isAuth) {
